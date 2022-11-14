3e206e6446b7784a4cf43aaac7d9cf60861fce35
PanovPavel <mactepnyb3@gmail.com>
2022-11-14 03:49:04 +0300

    добавлено модальное окно

---
+++ js/modaleBlock.js
@@ -0,0 +1,57 @@
+const modaleBlock = document.querySelector(".modal");
+const btnOpenModal = document.querySelectorAll("[data-openModal]");
+const btnCloseModal = document.querySelectorAll("[data-closeModal]");
+
+
+function openModal(){
+    modaleBlock.style.display = "block";
+    //Запретить сколл
+    document.body.style.overflow = "hidden";
+    clearTimeout(watchModalSecond);
+}
+function closeModal(){
+    modaleBlock.style.display = "none";
+    //Разрешить скролл
+    document.body.style.overflow = "";
+}
+
+btnOpenModal.forEach((item)=>{
+    item.addEventListener("click", (event)=>{
+        openModal();
+
+    })
+})
+
+btnCloseModal.forEach((item)=>{
+    item.addEventListener("click", (event)=>{
+        closeModal();
+    })
+})
+
+//Скрывать блок, если пользователь кликает не на него, а на класс modal
+modaleBlock.addEventListener("click", (event)=>{
+    if(event.target === modaleBlock){
+        closeModal();
+    }
+})
+//Закрытие блока с клавиатуры
+document.addEventListener("keyup", (event)=>{
+    if(event.code == "Escape"){
+        closeModal();
+    }
+})
+
+//Появления modal через 5 секунд
+let watchModalSecond = setTimeout(openModal, 5000);
+// console.log(document.documentElement.scrollHeight);
+// console.log(document.documentElement.scrollTop);
+
+
+
+window.addEventListener("scroll", function showModal(event){
+    if(((document.documentElement.scrollHeight - document.documentElement.clientHeight) - document.documentElement.scrollTop) < 5){
+        openModal();
+        //Удалить обработчик после исполнения
+        window.removeEventListener("scroll", showModal);
+    }
+})
