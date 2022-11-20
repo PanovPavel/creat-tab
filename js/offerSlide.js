2360436c66f8982d67a3cb5361bcb5caef996b5c
PanovPavel <mactepnyb3@gmail.com>
2022-11-21 01:46:55 +0300

    Добавлен простой слайдер картинок

---
+++ js/offerSlide.js
@@ -0,0 +1,38 @@
+const sliderWrapper = document.querySelectorAll(".offer__slider-wrapper .offer__slide");
+const currentNumber = document.querySelector("#current");
+const totalNumber = document.querySelector("#total");
+const lastSlider = document.querySelector(".offer__slider-prev");
+const next = document.querySelector(".offer__slider-next");
+
+let currentSlide = 1;
+
+currentNumber.textContent = "0" + currentSlide;
+totalNumber.textContent = "0" + sliderWrapper.length;
+
+lastSlider.addEventListener("click", ()=>{
+    if(currentSlide > 1){
+        --currentSlide;
+        currentNumber.textContent = "0" + currentSlide;
+        changeSlide();
+    }
+})
+next.addEventListener(`click`, ()=>{
+    if(currentSlide < sliderWrapper.length){
+        ++currentSlide;
+        currentNumber.textContent = "0" + currentSlide;
+        changeSlide();
+    }
+})
+
+
+changeSlide();
+function changeSlide() {
+    sliderWrapper.forEach(item=>{
+        item.style.display = "none";
+    })
+    sliderWrapper.forEach((item, index) => {
+        if (+(currentSlide - 1) === index) {
+            item.style.display = "block";
+        }
+    })
+}
