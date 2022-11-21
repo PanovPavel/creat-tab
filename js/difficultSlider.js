82789f21ba86fe8ecb5af2a7cc91e421762c0710
PanovPavel <mactepnyb3@gmail.com>
2022-11-21 22:50:29 +0300

    К слайдеру добавлена библиотека hammer.js для обработки события свайпа

--- js/difficultSlider.js
+++ js/difficultSlider.js
@@ -16,6 +16,27 @@ sliderInto.style.transition = `0.5s all`;
 sliderWrapper.style.overflow = `hidden`;
 
 nextButton.addEventListener(`click`, ()=>{
+    nextSlide();
+})
+
+new Hammer(sliderInto)
+    .on(`swipe`, (event)=>{
+        if(event.deltaX > 5){
+           lastSlide();
+        }else if(event.deltaX < -5){
+            nextSlide();
+        }
+        console.log(event.deltaX);
+    });
+lastSliderButton.addEventListener("click", ()=>{
+    lastSlide();
+})
+
+// changeSlide();
+// function changeSlide() {
+//
+// }
+function nextSlide(){
     if(currentSlide < sliderImg.length) {
         ++currentSlide;
         currentNumber.textContent = "0" + currentSlide;
@@ -26,8 +47,8 @@ nextButton.addEventListener(`click`, ()=>{
         currentNumber.textContent = "0" + currentSlide;
         sliderInto.style.transform = `translateX(-${650*(currentSlide-1)}px)`;
     }
-})
-lastSliderButton.addEventListener("click", ()=>{
+}
+function lastSlide(){
     if(currentSlide > 1){
         --currentSlide;
         currentNumber.textContent = "0" + currentSlide;
@@ -38,9 +59,4 @@ lastSliderButton.addEventListener("click", ()=>{
         currentNumber.textContent = "0" + currentSlide;
         sliderInto.style.transform = `translateX(-${650*(currentSlide-1)}px)`;
     }
-})
-
-changeSlide();
-function changeSlide() {
-
 }
